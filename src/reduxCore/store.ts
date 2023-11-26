3bf01494e9abf38d4769d0d28160381b367f697c
BFipe <ilya.maximov11@gmail.com>
2023-11-26 16:41:19 +0300

    Removed CounterSlice from project

--- src/reduxCore/store.ts
+++ src/reduxCore/store.ts
@@ -1,11 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "./features/counter/CounterSlice";
 import saveToLocalStorageMiddleware from "./middlewares/saveToLocalStorageMiddleware";
 import cartReducer from "./features/cart/CartSlice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     cart: cartReducer,
   },
   middleware: (getDefaultMiddleware) =>
